a2f321d98ea8eb6218825c2b2be1c70928eb5e66
Vinay C N <vinaycngowda@gmail.com>
2023-11-20 11:48:19 +0530

    inintal

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -2,9 +2,26 @@ package com.driver;
 
 public class Main {
     public static void main(String[] args) {
-        F1 mercedes = new F1("mercedes", false);
-        mercedes.move(40, 0);
-        mercedes.accelerate(20);
-        mercedes.accelerate(-60);
+//        F1 mercedes = new F1("mercedes", false);
+//        mercedes.move(40, 0);
+//        mercedes.accelerate(20);
+//        mercedes.accelerate(-60);
+        Car car = new Car("Tata", 4, 4, 4, true, "Sedan",0);
+        car.move(20, 30);
+
+        assert car.getCurrentSpeed() == 20;
+
+        assert car.getCurrentDirection() == 30;
+
+        car.steer(45);
+
+        assert car.getCurrentDirection() == 75;
+
+        assert car.getCurrentSpeed() == 20;
+
+        car.stop();
+
+        assert car.getCurrentSpeed() == 0;
+
     }
 }
